1b968f11dbaa0d518752ef02d2e7350cd1285a72
哈勃-慈含 <16178283@qq.com>
2026-03-30 13:51:23 +0800

    增加中文本地化语言支持

---
+++ lang/zh_cn/block_vitrinadb.php
@@ -0,0 +1,227 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_vitrinadb', language 'zh_cn'
+ *
+ * @package   block_vitrinadb
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['amountcourses'] = '课程数量';
+$string['amountcourses_help'] = '在主视图中要显示的课程数量。';
+$string['catalog'] = '课程目录';
+$string['categories'] = '课程类别';
+$string['categories_help'] = '选择要在主视图中显示课程的类别。';
+$string['catfilterview'] = '类别筛选视图';
+$string['catfilterview_default'] = '默认';
+$string['catfilterview_help'] = '选择用于显示课程类别筛选器的视图样式。';
+$string['catfilterview_tree'] = '树形';
+$string['channels'] = '频道筛选';
+$string['channels_help'] = '可选的频道列表；用逗号或分号（半角或全角）分隔多个频道名称。';
+$string['comment_by'] = '作者：{$a}';
+$string['commentslabel'] = '{$a} 条评论';
+$string['commentsmanager'] = '评论管理器';
+$string['commentsmanager_help'] = '选择用于课程评论的插件。';
+$string['commentsmodaltitle'] = '课程评论';
+$string['commentstitle'] = '最新评论';
+$string['commentstitlealternate'] = '课程评分';
+$string['codefield'] = '代码字段';
+$string['codefield_help'] = '用于按代码值排序的课程自定义字段。';
+$string['completed'] = '课程已完成';
+$string['completepercent'] = '已完成 {$a}%';
+$string['countstars'] = '{$a} 星';
+$string['coursecategory'] = '课程类别';
+$string['sourcecourse'] = '来源课程';
+$string['sourcecourse_help'] = '选择一个课程，其数据库活动将作为该区块中资源列表的来源。';
+$string['coursedetail'] = '课程详情';
+$string['coursedetailtitle'] = '课程：{$a->fullname}';
+$string['courselinkcopiedtoclipboard'] = '课程链接已复制到剪贴板';
+$string['coverimagetype'] = '封面图片';
+$string['coverimagetype_default'] = '默认区块图片';
+$string['coverimagetype_generated'] = '随机生成纹理';
+$string['coverimagetype_help'] = '当课程没有封面图片时使用的选项。';
+$string['coverimagetype_none'] = '无';
+$string['customgrenroll'] = '注册到所选分组';
+$string['customtitle'] = '自定义标题';
+$string['daystoupcoming'] = '即将开始课程的天数';
+$string['daystoupcoming_help'] = '在区块视图中，将课程视为“即将开始/进行中”的天数范围。';
+$string['daysago'] = '{$a} 天前';
+$string['decimalpoints'] = '小数位数';
+$string['decimalpoints_help'] = '费用格式中要使用的小数位数。';
+$string['defaultsort'] = '所有项目';
+$string['detailinfo'] = '详细信息说明';
+$string['detailinfo_help'] = '显示在课程详情页面中的通用说明信息。';
+$string['enroll'] = '选课/注册';
+$string['enrollavailablepremium'] = '仅高级用户可注册';
+$string['enrolled'] = '您已经选修了该课程！';
+$string['enrolledended'] = '您已选修本课程，但当前注册已失效';
+$string['enrollrequired'] = '您需要先选课才能学习本课程！';
+$string['feeinfo'] = '{$a}';
+$string['feeinfoinlist'] = '{$a}';
+$string['filtercustomfields'] = '用于筛选的自定义字段';
+$string['filtercustomfields_help'] = '选择用于在目录中筛选课程的自定义字段。
+仅支持下拉选择和复选框类型的字段。';
+$string['fulltextsearch'] = '全文搜索';
+$string['gotocourse'] = '进入课程';
+$string['greats'] = '精选条目';
+$string['hascourseview'] = '您当前账号可以访问此课程！';
+$string['htmlfooter'] = '区块页脚内容';
+$string['htmlheader'] = '区块页眉内容';
+$string['license-cc-0'] = '公共领域';
+$string['license-cc-by'] = '知识共享 署名 4.0 国际许可协议';
+$string['license-cc-by-nc'] = '知识共享 署名-非商业性使用 4.0 国际许可协议';
+$string['license-cc-by-nc-nd'] = '知识共享 署名-非商业性使用-禁止演绎 4.0 国际许可协议';
+$string['license-cc-by-nc-sa'] = '知识共享 署名-非商业性使用-相同方式共享 4.0 国际许可协议';
+$string['license-cc-by-nd'] = '知识共享 署名-禁止演绎 4.0 国际许可协议';
+$string['license-cc-by-sa'] = '知识共享 署名-相同方式共享 4.0 国际许可协议';
+$string['licensefield'] = '许可证字段';
+$string['licensefield_help'] = '用于存储课程许可证信息的课程字段。';
+$string['loadmore'] = '加载更多资源';
+$string['mediafield'] = '媒体字段';
+$string['mediafield_help'] = '用于存储课程视频 URL 的课程字段。';
+$string['newblocktitle'] = 'ShowcaseDb';
+$string['nocoursesview'] = '这里暂无可显示的资源。';
+$string['nomorecourses'] = '已经没有更多可显示的项目了';
+$string['notenrollable'] = '本课程当前不开放选课！';
+$string['notvisible'] = '本课程当前不可用';
+$string['opendetailstarget'] = '详情打开方式';
+$string['opendetailstarget_blank'] = '新窗口';
+$string['opendetailstarget_help'] = '选择资源详情页面的打开方式。';
+$string['opendetailstarget_self'] = '当前窗口';
+$string['paymentbutton'] = '支付课程';
+$string['paymentinfo'] = '可用于购买本课程的支付方式。';
+$string['paymentmethods'] = '支付方式';
+$string['paymentrequired'] = '本课程为收费课程';
+$string['paymenturlfield'] = '支付链接字段';
+$string['paymenturlfield_help'] = '';
+$string['pluginname'] = 'ShowcaseDb';
+$string['premium'] = '高级';
+$string['premiumcohort'] = '高级用户群组';
+$string['premiumcohort_help'] = '自助选课实例应关联到配置好的群组，以便高级用户可以注册。如果不希望在每个课程中单独配置群组，可以不在此选择群组，但应为自助选课设置密码，避免所有用户都能注册。';
+$string['premiumcoursefield'] = '高级课程字段';
+$string['premiumcoursefield_help'] = '用于标记“高级课程”类型的课程字段。
+如果未配置该字段，将不会单独处理高级课程，但仍可以有高级用户并允许其访问任何资源。';
+$string['premiumenrolledcourse'] = '用于识别高级用户的课程';
+$string['premiumenrolledcourse_help'] = '选修该课程的用户将被视为高级用户。';
+$string['premiumfield'] = '高级用户类型字段';
+$string['premiumfield_help'] = '';
+$string['premiumvalue'] = '高级用户字段值';
+$string['premiumvalue_help'] = '';
+$string['privacy:metadata'] = 'ShowcaseDb 区块本身不存储任何个人数据。';
+$string['ratingmanager'] = '评分管理器';
+$string['ratingmanager_help'] = '选择用于资源评分的插件。';
+$string['ratings'] = '评分';
+$string['ratingslabel'] = '{$a} 条评分';
+$string['rating_detail_title'] = '{$a->count} 条评分，平均 {$a->avg}';
+$string['recents'] = '最新条目';
+$string['relatedcourses'] = '相关课程';
+$string['relatedlimit'] = '相关课程数量上限';
+$string['relatedlimit_help'] = '在详情页面中要显示的相关课程数量。如果设置为 0，则不显示相关课程。';
+$string['requireauth'] = '您需要先<a href="{$a}">登录</a>，才能选修本课程';
+$string['returntocatalog'] = '返回目录';
+$string['selectcategories'] = '选择课程类别';
+$string['settingsheaderappearance'] = '外观';
+$string['settingsheaderfields'] = '课程字段';
+$string['settingsheaderfiltering'] = '筛选';
+$string['settingsheaderpayment'] = '与付费相关的选项';
+$string['sharecourse'] = '分享课程';
+$string['sharecoursedesc'] = '你是否认识可能会喜欢这门课程的人？<br />
+<strong>把它推荐给 TA 吧</strong>';
+$string['shopmanager'] = '商店管理器';
+$string['shopmanager_help'] = '选择用作“商店”的插件。';
+$string['showallcomments'] = '查看全部';
+$string['showcustomfields'] = '显示自定义字段';
+$string['showcustomfields_help'] = '在资源详情页面中显示的自定义字段。';
+$string['showicon'] = '仅图标';
+$string['showlesscomments'] = '收起评论';
+$string['showlongcustomfields'] = '显示长文本字段';
+$string['showlongcustomfields_help'] = '在资源详情页面的描述部分显示的自定义字段。';
+$string['showmore'] = '显示更多...';
+$string['showmorecomments'] = '显示更多评论';
+$string['showtext'] = '仅文字';
+$string['singleamountcourses'] = '单个视图数量';
+$string['singleamountcourses_help'] = '在区块默认视图中要显示的资源数量。';
+$string['socialnetworks'] = '社交网络';
+$string['socialnetworks_help'] = '每行配置一个网络，格式：icon-name|URL。
+URL 中可以使用以下占位符：{url}、{urlencoded} 和 {name}。';
+$string['sortalphabetically'] = '按字母排序';
+$string['sortbydefault'] = '默认排序';
+$string['sortbydefault_help'] = '选择在“所有条目”视图中资源的默认排序方式。';
+$string['sortbycode'] = '按代码排序';
+$string['sortbyfinishdate'] = '按结束日期排序';
+$string['sortbystartdate'] = '按开始日期排序';
+$string['sortdefault'] = '资源排序';
+$string['sortdirection'] = '排序方向';
+$string['sortdirection_help'] = '选择资源的排序方向。';
+$string['sortdirection_asc'] = '升序';
+$string['sortdirection_desc'] = '降序';
+$string['sortlabel'] = '排序类型';
+$string['staticfilters'] = '静态筛选器';
+$string['staticfilters_help'] = '选择要在目录中使用的固定筛选项。';
+$string['summary'] = '摘要';
+$string['summary_help'] = '该内容显示在资源列表和详情页面的顶部。';
+$string['syncpremiumenrolmentstask'] = '同步高级用户注册任务';
+$string['tabtitle_default'] = '全部条目';
+$string['tabtitle_greats'] = '精选条目';
+$string['tabtitle_premium'] = '高级条目';
+$string['tabtitle_recents'] = '即将发布的条目';
+$string['tabview'] = '标签视图';
+$string['tabview_help'] = '当有多个视图可用时，选择标签布局。';
+$string['templatetype'] = '模板类型';
+$string['templatetype_help'] = '选择一个模板来改变区块页面的外观。';
+$string['textandicon'] = '文字和图标';
+$string['tokenenroll'] = '使用口令注册';
+$string['viewall'] = '查看全部';
+$string['viewsicons'] = '视图图标';
+$string['viewsicons_help'] = '为各视图选择要显示的图标，通常用于标签菜单。
+每行一个，格式：viewname=typo/icon，例如：greats=i/star。如果未指定，将使用默认图标。
+可用视图：default、recents、greats、premium。';
+$string['vitrinadb:addinstance'] = '添加一个新的 ShowcaseDb 区块';
+$string['vitrinadb:myaddinstance'] = '在仪表盘中添加一个新的 ShowcaseDb 区块';
+$string['waiting'] = '等待中';
+
+// File types for shared resources.
+$string['filetype_audio'] = '音频';
+$string['filetype_video'] = '视频';
+$string['filetype_pdf'] = 'PDF';
+$string['filetype_office'] = 'Office';
+$string['filetype_presentation'] = 'PPT';
+$string['filetype_image'] = '图片';
+$string['filetype_other'] = '文件';
+$string['filetype_html'] = 'HTML';
+
+// Resource channels title for hover on filetype icon.
+$string['resource_channels_title'] = '类别：{$a}';
+
+// Pinned / prime / hidden resources.
+$string['pinnedlabel'] = '置顶';
+$string['primelabel'] = '推荐';
+$string['hidelabel'] = '隐藏';
+
+// Pending approval badge label.
+$string['pendinglabel'] = '等待审批';
+
+// Show status filter.
+$string['showstatusfilter'] = '显示状态';
+$string['showstatus_all'] = '全部';
+
+// Author filter.
+$string['authorfilter'] = '作者';
+$string['author_all'] = '全部';
+
+// Pending approval filter.
+$string['pendingfilter'] = '仅显示待审批记录';
